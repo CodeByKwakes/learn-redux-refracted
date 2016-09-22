d9cdee7a9fd3480dfd55fe4f85bde7e1c6c21a14
KwakesProject <kwakes@me.com>
2016-09-22 13:31:17 +0100

    video 19 about Redux Dev Tools complete

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -1,4 +1,4 @@
-import { createStore, compse } from 'redux'
+import { createStore, compose } from 'redux'
 import { syncHistoryWithStore } from 'react-router-redux'
 import { browserHistory } from 'react-router'
 
@@ -14,13 +14,18 @@ const defaultState = {
     comments
 }
 
+// Redux Dev Tools
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+)
+
 // dont need to do if the key and value both match 
 // const defaultState = {
 //     posts       : posts,
 //     comments    : comments
 // }
 
-const store = createStore(rootReducer, defaultState)
+const store = createStore(rootReducer, defaultState, enhancers)
 
 export const history = syncHistoryWithStore(browserHistory, store)
 
