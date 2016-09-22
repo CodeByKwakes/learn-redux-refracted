285d33ce357df2f72ddd512f582ae3581b78eb83
KwakesProject <kwakes@me.com>
2016-09-22 12:33:17 +0100

    video 18 out of 20 done

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -24,4 +24,12 @@ const store = createStore(rootReducer, defaultState)
 
 export const history = syncHistoryWithStore(browserHistory, store)
 
+// Hot reloading Redux Reducers with webpack
+if(module.hot){
+    module.hot.accept('./reducers/', () => {
+        const nextRootReducer = require('./reducers/index').default
+        store.replaceReducer(nextRootReducer)
+    })
+}
+
 export default store
