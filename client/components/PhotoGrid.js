8c7e79a08231e5277141db49fb9d2c9ef9a98bd6
KwakesProject <kwakes@me.com>
2016-09-23 14:36:35 +0100

    code added

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
 import Photo from './Photo'
 
 const PhotoGrid = React.createClass({
@@ -11,11 +11,23 @@ const PhotoGrid = React.createClass({
   }
 })
 
-/*const PhotoGrid = ({post}) => (
+/*const PhotoGrid = ({posts}) => (
   <div className="photo-grid">
-    {posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />) }
+    {posts.map((post, i) => <Photo {...post} key={i} i={i} post={post} />) }
   </div>
-)*/
+)
+
+PhotoGrid.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    code: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
+    display_src: PropTypes.string.isRequired
+  }).isRequired).isRequired
+}
+*/
+
 
 // Other way of writting the above code
 // export const Photo = () => (
@@ -27,8 +39,8 @@ const PhotoGrid = React.createClass({
 // )
 
 // cant use the below to display JSON format on the page
-  // <pre>
-  //   { JSON.stringify(this.props.posts, null, ' ') }
-  // </pre>
+// <pre>
+//   { JSON.stringify(this.props.posts, null, ' ') }
+// </pre>
 
 export default PhotoGrid;
